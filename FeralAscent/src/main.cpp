077f957f5f1f6ae7df655118343c1db802b9307b
neil-mckenna <70652115+neil-mckenna@users.noreply.github.com>
2024-10-18 12:19:34 +0100

    Initial Commit

---
+++ FeralAscent/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Blah.h"
+
+int main()
+{
+
+	//std::cout << "Hello world!" << std::endl;
+	Blah().boo();
+
+	return 0;
+}
---
+++ blah/src/Blah.cpp
@@ -0,0 +1,8 @@
+#include "Blah.h"
+#include <iostream>
+
+void Blah::boo()
+{
+	std::cout << "Boo" << std::endl;
+	
+}
